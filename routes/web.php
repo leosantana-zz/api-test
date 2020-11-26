8d2d929f83f5a2fc5328b7c14e80eb8870a058c7
Leonardo de Santana <contato@leosantana.com>
2020-11-26 13:50:49 -0300

    Home route returns message

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
+$router->get('/', function(){
+    return 'Take Home assignment';
+});
 $router->get('balance', 'AccountController@balance');
 $router->post('reset', 'AccountController@reset');
 $router->post('event', 'AccountController@event');
